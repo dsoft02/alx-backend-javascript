aed1c02fdfccb4fca287668ef3d3c3b0d61ed3d6
Ebenezer Ogidiolu <ogidioluebenezer@gmail.com>
2023-07-05 11:41:03 -0700

    Task 6. Creating functions specific to employees

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -38,10 +38,22 @@ class Teacher implements TeacherInterface {
   }
 }
 
-function createEmployee(salary: number | string): Director | Teacher {
+const createEmployee = (salary: number | string): Director | Teacher =>  {
   if (typeof salary === "number" && salary < 500) {
     return new Teacher();
   } else {
     return new Director();
+  }
+}
+
+const isDirector = (employee: Director | Teacher): employee is Director => {
+  return typeof (employee as Director).workDirectorTasks === "function";
+}
+
+const executeWork = (employee: Director | Teacher): string =>{
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  } else {
+    return employee.workTeacherTasks();
   }
 }
