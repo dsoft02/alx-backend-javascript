e0f5379590731ba2dde5851520c7d8cfd3970cb6
Ebenezer Ogidiolu <ogidioluebenezer@gmail.com>
2023-07-04 21:07:38 -0700

    Task 10. More map data structure

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,18 @@
+const updateUniqueItems = (map) => {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  const updatedMap = new Map();
+  for (const [key, value] of map.entries()) {
+    if (value === 1) {
+      updatedMap.set(key, 100);
+    } else {
+      updatedMap.set(key, value);
+    }
+  }
+
+  return updatedMap;
+};
+
+export default updateUniqueItems;
